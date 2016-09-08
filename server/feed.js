045028ad36cfea2ff00b9f90aff3567abc76e226
Eduardo Russo <russoedu@gmail.com>
2016-09-08 09:04:42 -0300

    [master]

--- server/feed.js
+++ server/feed.js
@@ -1,10 +1,8 @@
 module.exports = function(data) {
   var news = data.news;
-  for (var i in news) {
-    if (news.hasOwnProperty(i)) {
-      if (news[i].formatedDate > Date.now()) {
-        news[i] = null;
-      }
+  for (var i = 0; i < data.news.length; i++) {
+    if (news[i].formatedDate > Date.now()) {
+      news[i] = null;
     }
   }
   return data;
