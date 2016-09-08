9afc63f2b165db473d5dfa769c3a99ee20ff4c6e
Eduardo Russo <russoedu@gmail.com>
2016-09-08 09:07:09 -0300

    [master]

--- server/feed.js
+++ server/feed.js
@@ -1,7 +1,7 @@
 module.exports = function(data) {
   var news = data.news;
   for (var i = 0; i < data.news.length; i++) {
-    if (news[i].formatedDate > Date.now()) {
+    if (new Date(news[i].formatedDate) > Date.now()) {
       news[i] = null;
     }
   }
