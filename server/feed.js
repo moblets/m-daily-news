71146ae680395bb592d1de421a2b6989a15f5095
Eduardo Russo <russoedu@gmail.com>
2016-09-14 07:33:26 -0300

    [master] Corrected feed response to send only today news

--- server/feed.js
+++ server/feed.js
@@ -1,16 +1,17 @@
 	module.exports = function(data) {
-  for (var i = 0; i < data.news.length; i++) {
-		// Only send news older than today
-    var date = new Date(data.news[i].formatedDate);
-    data.news[i].theDate = date;
+  var news = data.news;
 
-    var today = new Date();
-    today.setHours(0, 0, 0, 0);
-    data.today = today;
+  var today = new Date();
+  today.setUTCHours(0, 0, 0, 0);
+  data.today = today;
 
-    // if (date !== today) {
-      // data.news.splice(i, 1);
-    // }
+  for (var i = news.length - 1; i >= 0; i--) {
+    var date = new Date(news[i].formatedDate);
+
+    // Only send news for today
+    if (date.getTime() !== today.getTime()) {
+      news.splice([i], 1);
+    }
   }
   return data;
 };
