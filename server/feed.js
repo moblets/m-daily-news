f2e0c117e48a070116513e28e373a14a7af8e7a5
Eduardo Russo <russoedu@gmail.com>
2016-09-14 06:52:43 -0300

    [master] .editorconfig and feed

--- server/feed.js
+++ server/feed.js
@@ -1,15 +1,16 @@
-module.exports = function(data) {
+	module.exports = function(data) {
   for (var i = 0; i < data.news.length; i++) {
 		// Only send news older than today
     var date = new Date(data.news[i].formatedDate);
-    data.theDate = date;
+    data.news[i].theDate = date;
 
     var today = new Date();
     today.setHours(0, 0, 0, 0);
     data.today = today;
-    if (date !== today) {
-      data.news.splice(i, 1);
-    }
+
+    // if (date !== today) {
+      // data.news.splice(i, 1);
+    // }
   }
   return data;
 };
