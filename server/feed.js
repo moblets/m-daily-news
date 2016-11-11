d3f52ea925d2448a73ec1a219d16b528852813bf
Eduardo Russo <russoedu@gmail.com>
2016-11-11 09:49:32 -0200

    [master] Testing

--- server/feed.js
+++ server/feed.js
@@ -1,66 +1,64 @@
 	module.exports = function(data) {
-    return data;
+  var news = data.news;
+
+  var today = new Date();
+  today.setUTCHours(0, 0, 0, 0);
+  data.today = today;
+
+  for (var i = news.length - 1; i >= 0; i--) {
+    var date = new Date(news[i].formatedDate);
+
+    // Only send news for today
+    if (date.getTime() === today.getTime()) {
+      news[i] = {
+        id: news[i].id,
+        date: news[i].formatedDate,
+        highlight: {
+          content: news[i].highlight,
+          used: false,
+          show: false,
+          link: news[i].link
+        },
+        more: {
+          content: news[i].more,
+          used: false,
+          entries: [
+            {
+              content: news[i].entries_content_1,
+              link: news[i].entries_link_1
+            },
+            {
+              content: news[i].entries_content_2,
+              link: news[i].entries_link_2
+            },
+            {
+              content: news[i].entries_content_3,
+              link: news[i].entries_link_3
+            },
+            {
+              content: news[i].entries_content_4,
+              link: news[i].entries_link_4
+            }
+          ]
+        },
+        next: {
+          content: news[i].next,
+          used: false
+        }
+      };
+      // Remove unused "more" news
+      for (var j = news[i].more.entries.length - 1; j >= 0; j--) {
+        if (news[i].more.entries[j].content === "") {
+          news[i].more.entries.splice([j], 1);
+        }
+      }
+      // If it has no "more" news, delete the "more" object
+      if (news[i].more.entries.length === 0) {
+        delete news[i].more;
+      }
+    } else {
+      news.splice([i], 1);
+    }
   }
-//   var news = data.news;
-//
-//   var today = new Date();
-//   today.setUTCHours(0, 0, 0, 0);
-//   data.today = today;
-//
-//   for (var i = news.length - 1; i >= 0; i--) {
-//     var date = new Date(news[i].formatedDate);
-//
-//     // Only send news for today
-//     if (date.getTime() === today.getTime()) {
-//       news[i] = {
-//         id: news[i].id,
-//         date: news[i].formatedDate,
-//         highlight: {
-//           content: news[i].highlight,
-//           used: false,
-//           show: false,
-//           link: news[i].link
-//         },
-//         more: {
-//           content: news[i].more,
-//           used: false,
-//           entries: [
-//             {
-//               content: news[i].entries_content_1,
-//               link: news[i].entries_link_1
-//             },
-//             {
-//               content: news[i].entries_content_2,
-//               link: news[i].entries_link_2
-//             },
-//             {
-//               content: news[i].entries_content_3,
-//               link: news[i].entries_link_3
-//             },
-//             {
-//               content: news[i].entries_content_4,
-//               link: news[i].entries_link_4
-//             }
-//           ]
-//         },
-//         next: {
-//           content: news[i].next,
-//           used: false
-//         }
-//       };
-//       // Remove unused "more" news
-//       for (var j = news[i].more.entries.length - 1; j >= 0; j--) {
-//         if (news[i].more.entries[j].content === "") {
-//           news[i].more.entries.splice([j], 1);
-//         }
-//       }
-//       // If it has no "more" news, delete the "more" object
-//       if (news[i].more.entries.length === 0) {
-//         delete news[i].more;
-//       }
-//     } else {
-//       news.splice([i], 1);
-//     }
-//   }
-//   return data;
-// };
+  return data;
+};
